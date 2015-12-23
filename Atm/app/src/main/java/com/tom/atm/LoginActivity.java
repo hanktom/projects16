77733bdde54c1efd6a2c5bb8a533bc406e02bcc0
Hank Tom <hanktom@gamil.com>
2015-12-23 23:16:15 +0800

    登入畫面與功能

---
+++ Atm/app/src/main/java/com/tom/atm/LoginActivity.java
@@ -0,0 +1,51 @@
+package com.tom.atm;
+
+import android.app.AlertDialog;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+    public void login(View v){
+        EditText edUserid = (EditText) findViewById(R.id.userid);
+        EditText edPasswd = (EditText) findViewById(R.id.passwd);
+        String uid = edUserid.getText().toString();
+        String pw = edPasswd.getText().toString();
+        if (uid.equals("jack") && pw.equals("1234")){ //登入成功
+            Toast.makeText(this, "登入成功", Toast.LENGTH_LONG).show();
+            finish();
+        }else{  //登入失敗
+            new AlertDialog.Builder(this)
+                    .setTitle("Atm")
+                    .setMessage("登入失敗")
+                    .setPositiveButton("OK", null)
+                    .show();
+        }
+    }
+    public void cancel(View v){
+
+    }
+}
