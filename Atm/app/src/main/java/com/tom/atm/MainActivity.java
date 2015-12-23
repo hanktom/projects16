79625dfd4514bafa9f9596fa6cfed721521594e6
Hank Tom <hanktom@gamil.com>
2015-12-24 00:18:30 +0800

    5-7轉換Activity取得結果

--- Atm/app/src/main/java/com/tom/atm/MainActivity.java
+++ Atm/app/src/main/java/com/tom/atm/MainActivity.java
@@ -6,19 +6,22 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
     boolean logon = false;
+    public static final int FUNC_LOGIN = 1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         if (!logon){  //如果未登入, 則開啟LoginActivity
             Intent intent = new Intent(this, LoginActivity.class);
-            startActivity(intent);
+//            startActivity(intent);
+            startActivityForResult(intent, FUNC_LOGIN);
         }
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
@@ -31,6 +34,20 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == FUNC_LOGIN){
+            if (resultCode == RESULT_OK){
+                String uid = data.getStringExtra("LOGIN_USERID");
+                String pw = data.getStringExtra("LOGIN_PASSWD");
+                Log.d("RESULT", uid + "/" + pw);
+            }else{
+                finish();
+            }
+        }
     }
 
     @Override
