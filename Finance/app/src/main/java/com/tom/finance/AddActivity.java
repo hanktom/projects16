7db39957444fab9cc88876201f3019aebe2d896c
Hank Tom <hanktom@gamil.com>
2016-01-06 23:49:17 +0800

    Finance專案準備

---
+++ Finance/app/src/main/java/com/tom/finance/AddActivity.java
@@ -0,0 +1,41 @@
+package com.tom.finance;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.EditText;
+
+public class AddActivity extends AppCompatActivity {
+    private EditText edDate;
+    private EditText edInfo;
+    private EditText edAmount;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add);
+        findViews();
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+    private void findViews() {
+        edDate = (EditText) findViewById(R.id.ed_date);
+        edInfo = (EditText) findViewById(R.id.ed_info);
+        edAmount = (EditText) findViewById(R.id.ed_amount);
+    }
+
+    public void add(View v){
+
+    }
+}
