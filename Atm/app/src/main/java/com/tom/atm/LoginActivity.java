92b5bf02c991dd91210a2c1d05d6204c75a5ceb3
Hank Tom <hanktom@gamil.com>
2016-01-16 14:33:34 +0800

    網路登入功能

--- Atm/app/src/main/java/com/tom/atm/LoginActivity.java
+++ Atm/app/src/main/java/com/tom/atm/LoginActivity.java
@@ -2,14 +2,22 @@ package com.tom.atm;
 
 import android.app.AlertDialog;
 import android.content.SharedPreferences;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -38,7 +46,15 @@ public class LoginActivity extends AppCompatActivity {
         EditText edPasswd = (EditText) findViewById(R.id.passwd);
         String uid = edUserid.getText().toString();
         String pw = edPasswd.getText().toString();
-        if (uid.equals("jack") && pw.equals("1234")){ //登入成功
+        String url = new StringBuilder("http://atm.snpy.org/login?uid=")
+                .append(uid)
+                .append("&pw=")
+                .append(pw)
+                .toString();
+        new LoginTask().execute(url);
+        //請註解以下程式碼
+
+        /*if (uid.equals("jack") && pw.equals("1234")){ //登入成功
             SharedPreferences setting = getSharedPreferences("atm", MODE_PRIVATE);
             setting.edit()
                     .putString("PREF_USERID", uid)
@@ -54,8 +70,47 @@ public class LoginActivity extends AppCompatActivity {
                     .setMessage("登入失敗")
                     .setPositiveButton("OK", null)
                     .show();
+        }*/
+    }
+
+    class LoginTask extends AsyncTask<String, Void, Boolean>{
+
+        @Override
+        protected Boolean doInBackground(String... params) {
+            boolean logon = false;
+            try {
+                URL url = new URL(params[0]);
+                InputStream is = url.openStream();
+                int data = is.read();
+                Log.d("HTTP", String.valueOf(data));
+                if (data == 49){
+                    return true;
+                }
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return logon;
+        }
+
+        @Override
+        protected void onPostExecute(Boolean logon) {
+            super.onPostExecute(logon);
+            if (logon) {
+                //儲存帳號至SharedPreferences
+                //結束本畫面, 回到MainActivity
+                finish();
+            }else {
+                new AlertDialog.Builder(LoginActivity.this)
+                        .setTitle("Atm")
+                        .setMessage("登入失敗")
+                        .setPositiveButton("OK", null)
+                        .show();
+            }
         }
     }
+
     public void cancel(View v){
 
     }
